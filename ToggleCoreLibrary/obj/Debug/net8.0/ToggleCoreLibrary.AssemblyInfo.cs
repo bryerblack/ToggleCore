fc8964ca47adaadeba7081b163375e585d3e6520
unknown <pedro.damiao@ccc.ufcg.edu.br>
2024-09-26 21:47:22 -0300

    refactor: fixed version

--- ToggleCoreLibrary/obj/Debug/net8.0/ToggleCoreLibrary.AssemblyInfo.cs
+++ ToggleCoreLibrary/obj/Debug/net8.0/ToggleCoreLibrary.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToggleCoreLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1+4a9098b24aaec3f95399730981ba449ecaf8c3aa")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.2.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.2+88a1b4f44b02b5d60345554e923a03a0b1258db6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToggleCoreLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToggleCoreLibrary")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.2.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
